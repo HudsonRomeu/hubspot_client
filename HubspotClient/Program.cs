0154fb94a44c9cf111c20b7570e1cc19fcd8f7e6
code-informatica <suporte@codeinformatica.com.br>
2020-06-02 10:16:18 -0300

    Upload code

---
+++ HubspotClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HubspotClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Test().Get();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HubspotClient/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace HubspotClient
+{
+    class Test
+    {
+        public async void Get()
+        {
+            try
+            {
+                var httpClient = new HttpClient();
+                var uri = new Uri("https://api.hubapi.com/crm/v3/objects/companies?hapikey=b4854144-aa95-41bb-b016-b917cb696b3f");
+                var response = await httpClient.GetAsync(uri);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
